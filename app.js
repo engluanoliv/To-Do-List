0260dcabf3f39bee1faa063f263193181998eaff
Luan Oliveira <franciscoluan123@hotmail.com>
2021-08-21 18:49:21 -0300

    New

--- app.js
+++ app.js
@@ -154,9 +154,9 @@ app.get("/about", function (req, res) {
 });
 
 let port = process.env.PORT;
-if (port == null || port == "" {
+if (port == null || port == "") {
     port = 3000;
-});
+};
 
 app.listen(port, function() {
   console.log("Server is started succesfully.");
